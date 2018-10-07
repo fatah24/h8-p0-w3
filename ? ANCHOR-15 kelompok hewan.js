0a9ecce0b0f3e0ace077f6554d05f3ee0ce6ed78
fatah banan wijaya <43367056+fatah24@users.noreply.github.com>
2018-10-07 13:42:31 +0700

    Update ? ANCHOR-15 kelompok hewan.js

--- ? ANCHOR-15 kelompok hewan.js
+++ ? ANCHOR-15 kelompok hewan.js
@@ -28,6 +28,6 @@ function groupAnimals(animals) {
   // TEST CASES
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 //   // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-//   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
 //   // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
   
