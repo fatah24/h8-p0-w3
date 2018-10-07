743c7846a2087a827795643acee54b3439eea910
fatah banan wijaya <43367056+fatah24@users.noreply.github.com>
2018-10-07 13:04:44 +0700

    Update ? ANCHOR-15 kelompok hewan.js

--- ? ANCHOR-15 kelompok hewan.js
+++ ? ANCHOR-15 kelompok hewan.js
@@ -9,7 +9,7 @@ function groupAnimals(animals) {
              kelompok.push(animals[i])
 
              if(i === animals.length -2){
-              kelompok.push(animals[i+1])
+              kelompok.push(animals[i])
               result.push(kelompok)
                 }
              
